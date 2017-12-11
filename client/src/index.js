e313ca9ce06937443adb94b75856a46e37ce9605
Brian Taylor <taylor.brian93@gmail.com>
2017-12-10 23:22:46 -0500

    Added connections to sendgrid

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import reducers from './reducers'
-import reduxThunk from 'redux-thunk'
+import reduxThunk from 'redux-thunk';
 
+import reducers from './reducers';
 import App from './components/App';
+
+import axios from 'axios';
+
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
 const requireCredits = require('../middleware/requireCredits');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
+const Mailer = require('../services/Mailer');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting');
+  });
+
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {title, subject, body, recipients } = req.body;
     //const user = await new User({googleId: profile.id}).save();
     const survey = new Survey({
@@ -15,6 +22,21 @@ module.exports = app => {
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
-    })
+    });
+
+    // Send mail
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+     await mailer.send();
+     await survey.save();
+     req.user.credits -= 1;
+     const user = await req.user.save();
+
+     res.send(user);
+   } catch (err) {
+     res.status(422).send(err);
+   }
+
   });
 };
